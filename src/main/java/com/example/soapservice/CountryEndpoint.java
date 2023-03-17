c5f97570e488b732f7c3cd4eb7bfa64862b7c889
robert-virtual <robertocastill63@gmail.com>
2023-03-17 14:36:39 -0600

    done

--- src/main/java/com/example/soapservice/CountryEndpoint.java
+++ src/main/java/com/example/soapservice/CountryEndpoint.java
@@ -6,17 +6,46 @@ import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
+import java.util.List;
+
 @Endpoint
 @RequiredArgsConstructor
 public class CountryEndpoint {
     public static final String NAMESPACE_URI = "http://soapservice.example.com";
-   private final CountryRepo countryRepo;
+    private final CountryRepo countryRepo;
 
-   @PayloadRoot(namespace = NAMESPACE_URI,localPart = "getCountryRequest")
-   @ResponsePayload
-   public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+    @ResponsePayload
+    public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
         GetCountryResponse response = new GetCountryResponse();
-        response.setCountry(countryRepo.findByName(request.name));
+
+        CountryModel countryModel = countryRepo.findByName(request.name);
+        Country country = new Country();
+        country.setId(countryModel.getId());
+        country.setName(countryModel.getName());
+        country.setPopulation(countryModel.getPopulation());
+        country.setCapital(countryModel.getCapital());
+        response.setCountry(country);
+        return response;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountriesRequest")
+    @ResponsePayload
+    public GetCountriesResponse getCountries() {
+        GetCountriesResponse response = new GetCountriesResponse();
+
+        List<CountryModel> data = countryRepo.findAll();
+        List<Country> countries = data.stream().map(x -> {
+            Country country = new Country();
+            country.setId(x.getId());
+            country.setCurrency(x.getCurrency());
+            country.setCapital(x.getCapital());
+            country.setName(x.getName());
+            country.setPopulation(x.getPopulation());
+            return country;
+        }).toList();
+        System.out.println(countries);
+        response.getCountries().addAll(countries);
         return response;
     }
 }
--- src/main/java/com/example/soapservice/CountryRepo.java
+++ src/main/java/com/example/soapservice/CountryRepo.java
@@ -3,5 +3,5 @@ package com.example.soapservice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CountryRepo extends JpaRepository<CountryModel,Long> {
-    Country findByName(String name);
+    CountryModel findByName(String name);
 }
