dff43ab77127906cebbfd5f5397443c02d524cab
robert-virtual <robertocastill63@gmail.com>
2023-03-17 10:36:44 -0600

    init countries

--- src/main/java/com/example/soapservice/CountryRepository.java
+++ src/main/java/com/example/soapservice/CountryRepository.java
@@ -1,9 +1,22 @@
 package com.example.soapservice;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class CountryRepository {
     private static final Map<String,Country> countries = new HashMap<>();
+    @PostConstruct
+    public void initData(){
+        Country honduras = new Country();
+        honduras.setName("Honduras");
+        honduras.setCapital("Tegucigalpa");
+        honduras.setPopulation(9459440);
+        honduras.setCurrency(Currency.LPS);
+        countries.put(honduras.getName(),honduras);
+    }
 
 }
