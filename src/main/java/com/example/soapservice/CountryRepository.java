e4c4885599d8c08f33e1c3b3687b0d5d42a9d9f1
robert-virtual <robertocastill63@gmail.com>
2023-03-17 10:13:57 -0600

    create repository and reference Country class

---
+++ src/main/java/com/example/soapservice/CountryRepository.java
@@ -0,0 +1,8 @@
+package com.example.soapservice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountryRepository {
+    private static final Map<String,Country> countries = new HashMap<>();
+}
